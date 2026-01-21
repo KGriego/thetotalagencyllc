332877ecd994ad93b041ab298100c350b0e3d711
Kevin <kev.gri32@gmail.com>
2026-01-21 02:46:24 -0800

    updating home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,33 +8,34 @@ import MoreStories from "./more-stories";
 
 import { getAllPosts } from "@/lib/api";
 import { CMS_NAME, CMS_URL } from "@/lib/constants";
+import { Intro } from "@/lib/components/about_us";
 
-function Intro() {
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
-      </h2>
-    </section>
-  );
-}
+// function Intro() {
+//   return (
+//     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+//       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+//         Blog.
+//       </h1>
+//       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
+//         A statically generated blog example using{" "}
+//         <a
+//           href="https://nextjs.org/"
+//           className="underline hover:text-success duration-200 transition-colors"
+//         >
+//           Next.js
+//         </a>{" "}
+//         and{" "}
+//         <a
+//           href={CMS_URL}
+//           className="underline hover:text-success duration-200 transition-colors"
+//         >
+//           {CMS_NAME}
+//         </a>
+//         .
+//       </h2>
+//     </section>
+//   );
+// }
 
 function HeroPost({
   title,
@@ -77,15 +78,16 @@ function HeroPost({
 }
 
 export default async function Page() {
-  const { isEnabled } = draftMode();
+  const { isEnabled } = await draftMode();
   const allPosts = await getAllPosts(isEnabled);
+
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
 
   return (
     <div className="container mx-auto px-5">
       <Intro />
-      {heroPost && (
+      {/* {heroPost && (
         <HeroPost
           title={heroPost.title}
           coverImage={heroPost.coverImage}
@@ -94,8 +96,8 @@ export default async function Page() {
           slug={heroPost.slug}
           excerpt={heroPost.excerpt}
         />
-      )}
-      <MoreStories morePosts={morePosts} />
+      )} */}
+      {/* <MoreStories morePosts={morePosts} /> */}
     </div>
   );
 }
